e532c818efa559c9cefb3a439860d4bd80c5924f
Greg Weber <greg@gregweber.info>
2013-06-05 15:47:23 -0700

    creators for queue emitter and workers
    use namespaced data field for user data

--- Database/MongoDB/Queue.hs
+++ Database/MongoDB/Queue.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE OverloadedStrings, FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings, FlexibleContexts, RecordWildCards #-}
 module Database.MongoDB.Queue (
     work
   , emit
+  , createEmitter
+  , createWorker
 ) where
 
 import Prelude hiding (lookup)
@@ -10,10 +12,12 @@ import Control.Monad.IO.Class (MonadIO)
 import Control.Applicative (Applicative)
 import Control.Monad.Trans.Control (MonadBaseControl(..))
 import Data.Text (Text)
+import Network.BSD (getHostName, HostName)
 
-queueCollection, handled, _id :: Text
+queueCollection, handled, dataField, _id :: Text
 queueCollection = "queue"
 handled = "handled"
+dataField = "data"
 _id = "_id"
 
 -- from Database.Persist.MongoDB, trying to move into driver
@@ -43,14 +47,49 @@ run act = do
   m1 <- access pipe master "mongomq" act
   print m1
 
-emit :: Document -> IO ()
-emit doc = run $ insert_ queueCollection doc
 
-work :: (Document -> Action IO ()) -> IO ()
-work handler = run $ do
+data QueueEmitter = QueueEmitter {
+                      qeVersion :: Int -- ^ version
+                    , qeHost :: HostName -- ^ HostName
+                    }
+
+data EmitterOpts = EmitterOpts { version :: Int }
+
+createEmitter :: IO QueueEmitter
+createEmitter = mkEmitter $ EmitterOpts { version = 1 }
+
+mkEmitter :: EmitterOpts -> IO QueueEmitter
+mkEmitter opts = do
+  name <- getHostName
+  return $ QueueEmitter (version opts) name 
+
+emit :: QueueEmitter -> Document -> IO ()
+emit QueueEmitter {..} doc =
+  run $ insert_ queueCollection [
+            "version" =: qeVersion
+          , dataField =: doc
+          , handled =: False
+          , "host" =: qeHost
+          ]
+          -- localTime: dt, 
+          -- globalTime: new Date(dt-self.serverTimeOffset),
+          -- pickedTime: new Date(dt-self.serverTimeOffset),
+
+data QueueWorker = QueueWorker
+data WorkerOpts = WorkerOpts
+
+createWorker :: IO QueueWorker
+createWorker = mkWorker $ WorkerOpts
+
+mkWorker :: WorkerOpts -> IO QueueWorker
+mkWorker _ = do
+  return $ QueueWorker
+
+work :: QueueWorker -> (Document -> Action IO ()) -> IO ()
+work QueueWorker {..} handler = run $ do
   cursor <- find (select [ handled =: False ] queueCollection) { options = [TailableCursor, AwaitData] }
   tailableCursorApply cursor $ \doc -> do
-    handler doc
+    handler (at "data" doc)
     findAndModifyOne queueCollection (at _id doc) [ handled =: True ]
   where
     tailableCursorApply :: (MonadIO m, MonadBaseControl IO m, Functor m) => Cursor -> (Document ->  Action m a) ->  Action m a
